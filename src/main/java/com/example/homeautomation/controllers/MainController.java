1578fcf9136d03daad3263c80f7eb661218b8e42
unknown <mtushar78@gmail.com>
2021-12-11 01:04:06 +0600

    jenkinhgtfhgf

--- src/main/java/com/example/homeautomation/controllers/MainController.java
+++ src/main/java/com/example/homeautomation/controllers/MainController.java
@@ -38,7 +38,7 @@ public class MainController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello  ghjyrtrt r All people !";
+        return "Hello All people !";
     }
     @GetMapping("/getUserInfo/{email}")
     public ResponseEntity<User> getUserInfo(@PathVariable String email){
